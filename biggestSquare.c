e4aac4bdf58f6e3053b58ceb7f1495c86ed5222b
Ahmed Galadi <galadiahmed99@gmail.com>
2025-09-20 02:39:04 +0100

    structure and functions

---
+++ biggestSquare.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int	minPos(int top, int left, int top_left) {
+	int output;
+
+	return (output);
+}
+
+char **isValidMap(char *map) {
+	char **output;
+
+	return (output);
+}
+
+char **findBSQ(char **map, char obstacle, char empty) {
+	char **output;
+
+	return (output);
+}
+
+int main(int argc, char *argv[]) {
+	
+	return (0);
+}
